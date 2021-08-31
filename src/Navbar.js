db8da29454b52c05d1954632fcb8793509aeac2a
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2021-08-31 17:23:59 +0500

    only add comment on navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
-import { Link} from 'react-router-dom';
+// navbar component
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-            <h1>The AD Blog</h1></Link>
+                <h1>The AD Blog</h1></Link>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
